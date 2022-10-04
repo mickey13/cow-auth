8b056896c55018421c5b4ad3f3db76973c26ce3b
Mickey Cowden <mickey@cowden.tech>
2022-10-03 20:47:32 -0400

    Regenerate project files.

--- lib/cow_auth.rb
+++ lib/cow_auth.rb
@@ -1,12 +1,14 @@
-require 'cow_auth/version'
-require 'cow_auth/user'
-require 'cow_auth/exceptions'
-require 'cow_auth/session_auth/session_endpoints'
-require 'cow_auth/session_auth/authenticate_request'
-require 'cow_auth/token_auth/session_endpoints'
-require 'cow_auth/token_auth/authenticate_request'
+require_relative 'cow_auth/version'
+require_relative 'cow_auth/user'
+require_relative 'cow_auth/exceptions'
+require_relative 'cow_auth/session_auth/session_endpoints'
+require_relative 'cow_auth/session_auth/authenticate_request'
+require_relative 'cow_auth/token_auth/session_endpoints'
+require_relative 'cow_auth/token_auth/authenticate_request'
 
 module CowAuth
+  class Error < StandardError; end
+
   def self.moo
     puts 'Moo Cow: ' + CowAuth::VERSION
     return CowAuth::User.new
--- test/test_cow_auth.rb
+++ test/test_cow_auth.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CowAuthTest < Minitest::Test
+class TestCowAuth < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::CowAuth::VERSION
   end
